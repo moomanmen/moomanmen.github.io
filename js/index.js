d7460311dca70b4e70867b6d177918b9e5f41f5d
Christian McAllister <moomanmen@yahoo.com>
2018-03-22 23:41:39 -0500

    start

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$("document").ready(function() {
+  var close = document.getElementsByClassName("closebtn");
+  var i;
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.opacity = "0";
+      setTimeout(function() {
+        div.style.display = "none";
+      }, 600);
+    };
+  }
+  $("#login").click(function() {
+       $("#warning").css("display", "none");
+   document.getElementById("success").style.display = "block";
+  });
+  $(document).keydown(function() {
+    var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+    var mail = $("#name").val();
+    var pass = $("#psw").val();
+    if ((pattern.test(mail)) && (pass != '')) {
+   $("#login").removeAttr("disabled");
+   $("#login").css('color','#4CAF50');
+    }
+  });
+});
